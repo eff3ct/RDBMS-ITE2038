35a81f0de52d32fe74e365ac6f0f219c11c6eb5b
최철민 <effect2110@hanyang.ac.kr>
2022-09-16 00:41:44 +0900

    MODIFY : 03.sql, add >= 3

--- project1/03.sql
+++ project1/03.sql
@@ -1,6 +1,12 @@
 SELECT T.name, C.nickname, MAX(C.level)
 FROM Trainer T, caughtPokemon C
 WHERE T.id = C.owner_id
+AND T.id IN (
+    SELECT owner_id
+    FROM caughtPokemon
+    GROUP BY owner_id
+    HAVING COUNT(*) >= 3
+)
 AND C.level = (
     SELECT MAX(CC.level)
     FROM caughtPokemon CC
