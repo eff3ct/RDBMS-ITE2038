bd04aedd2c727fdc2547fd46f8c89f8cb51b9255
최철민 <effect2110@hanyang.ac.kr>
2022-09-16 13:20:16 +0900

    MODIFY : 32.sql, add condition (https://piazza.com/class/l7h5ji4sv9c4gj/post/18)

--- project1/32.sql
+++ project1/32.sql
@@ -2,14 +2,27 @@ SELECT T.name, T.hometown
 FROM Trainer T, Gym G, caughtPokemon C
 WHERE T.id = G.leader_id
 AND T.id = C.owner_id
-AND C.pid IN (
-    SELECT E.after_id
-    FROM Evolution E, Pokemon P1, Pokemon P2
-    WHERE P1.id = E.before_id
-    AND P2.id = E.after_id
-    AND P1.id NOT IN (
-        SELECT after_id
-        FROM Evolution
+AND (
+    C.pid IN (
+        SELECT E.after_id
+        FROM Evolution E, Pokemon P1, Pokemon P2
+        WHERE P1.id = E.before_id
+        AND P2.id = E.after_id
+        AND P1.id NOT IN (
+            SELECT after_id
+            FROM Evolution
+        )
+    )
+    OR C.pid IN (
+        SELECT P.id
+        FROM Pokemon P
+        WHERE P.id NOT IN (
+            SELECT after_id
+            FROM Evolution
+            UNION
+            SELECT before_id
+            FROM Evolution
+        )
     )
 )
 GROUP BY T.name, T.hometown
