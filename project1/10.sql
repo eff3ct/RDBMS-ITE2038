9f4934be60f51e202d1bb2baede7f263a0cabcb0
최철민 <effect2110@hanyang.ac.kr>
2022-09-16 13:52:42 +0900

    MODIFY : 10.sql, add condition of the count of pokemon type

--- project1/10.sql
+++ project1/10.sql
@@ -1,4 +1,4 @@
-SELECT P.type, P.name
+SELECT P.type
 FROM Pokemon P
 WHERE P.id IN (
     SELECT before_id
@@ -7,4 +7,18 @@ WHERE P.id IN (
     SELECT after_id
     FROM Evolution
 )
+AND P.type IN (
+    SELECT P2.type
+    FROM Pokemon P2
+    WHERE P2.id IN (
+        SELECT before_id
+        FROM Evolution
+        UNION
+        SELECT after_id
+        FROM Evolution
+    )
+    GROUP BY P2.type
+    HAVING COUNT(P2.type) >= 3
+)
+GROUP BY P.type
 ORDER BY P.type;
