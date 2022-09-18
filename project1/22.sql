b2462a59eae99ee99ac9e2e03b6d5a512ea5ea2b
최철민 <effect2110@hanyang.ac.kr>
2022-09-18 09:26:58 +0900

    MODIFY : 22.sql, fix incorrect part.

--- project1/22.sql
+++ project1/22.sql
@@ -1,9 +1,11 @@
 SELECT T.name
-FROM Trainer T, caughtPokemon C1, caughtPokemon C2, Pokemon P
+FROM Trainer T, caughtPokemon C1, caughtPokemon C2, Pokemon P1, Pokemon P2
 WHERE T.id = C1.owner_id
 AND T.id = C2.owner_id
 AND C1.id <> C2.id
-AND C1.pid = C2.pid
-AND C1.pid = P.id
-AND P.type = 'Water'
+AND C1.pid = P1.id
+AND C2.pid = P2.id
+AND P1.type = 'Water'
+AND P2.type = 'Water'
+GROUP BY T.name
 ORDER BY T.name;
